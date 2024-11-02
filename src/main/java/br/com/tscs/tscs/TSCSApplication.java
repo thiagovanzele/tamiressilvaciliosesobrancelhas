6ee46c5f786bd6e71b281ca17c2b78b35046208a
Thiago Vanzele <thiagovanzele@gmail.com>
2024-11-02 16:49:05 -0300

    Project created

---
+++ src/main/java/br/com/tscs/tscs/TSCSApplication.java
@@ -0,0 +1,13 @@
+package br.com.tscs.tscs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TSCSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TSCSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tscs/tscs/TSCSApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tscs.tscs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TSCSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
